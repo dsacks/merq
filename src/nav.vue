6451fbc1a92be1845a031c696ca2be6776a073e0
Dennis Sacks <dsacks@glynlyon.com>
2018-07-27 15:40:59 -0700

    adding initial nav component

---
+++ src/nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <select >
+    <option v-for="item in items" value="{{item.value}}">{{item.name}}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'item',
+  data () {
+    return {
+       items: [
+           { value: 'v1', name: 'volume1'},
+           { value: 'v2', name: 'volume2'},
+           { value: 'v3', name: 'volume3'},
+           { value: 'v4', name: 'volume4'},
+       ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
